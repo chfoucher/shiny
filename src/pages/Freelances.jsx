9172d24cf722a337ee948498303c87b93b144e59
chfoucher <pele.foucher@free.fr>
2023-01-26 21:33:43 +0100

    Utilisation d'APIs

--- src/pages/Freelances.jsx
+++ src/pages/Freelances.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '../utils/Atoms';
 import styled from 'styled-components';
 import '../App.css';
 import Card from '../components/Card';
@@ -8,35 +10,40 @@ const CardsContainer = styled.div`
   grid-template-columns: repeat(2, 1fr);
 `;
 
-const freelanceProfiles = [
-  {
-    name: 'Jane Doe',
-    jobTitle: 'Devops',
-  },
-  {
-    name: 'John Doe',
-    jobTitle: 'Developpeur frontend',
-  },
-  {
-    name: 'Jeanne Biche',
-    jobTitle: 'Développeuse Fullstack',
-  },
-];
-
 function Freelances() {
+  const [isDataLoading, setDataLoading] = useState(true);
+  const [freelanceProfiles, setFreelanceProfiles] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:8000/freelances');
+        const { freelancersList } = await response.json();
+        setFreelanceProfiles(freelancersList);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setDataLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-      <CardsContainer>
-        {freelanceProfiles.map((profile, index) => (
-          <Card
-            key={`${profile.name}-${index}`}
-            label={profile.jobTitle}
-            picture={profile.picture}
-            title={profile.name}
-          />
-        ))}
-      </CardsContainer>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <CardsContainer>
+          {freelanceProfiles.map((profile) => (
+            <Card
+              key={`${profile.name}-${profile.id}`}
+              label={profile.job}
+              picture={profile.picture}
+              title={profile.name}
+            />
+          ))}
+        </CardsContainer>
+      )}
     </div>
   );
 }
--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,19 +1,41 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Loader } from '../utils/Atoms';
 
 function Survey() {
-    const { questionNumber } = useParams();
-    const questionIndex = Number.parseInt(questionNumber);
-    return (
-        <div>
-            <h1>Questionnaire 🧮</h1>
-            <h2>Question {questionIndex}</h2>
-            <nav>
-            { (questionIndex > 1) && <Link to={`/survey/${questionIndex - 1}`}>Précédente</Link> } 
-            { (questionIndex < 10) && <Link to={`/survey/${questionIndex + 1}`}>Suivante</Link> } 
-            { (questionIndex === 10) && <Link to="/results">Résultats</Link> } 
-        </nav>
-        </div>
-    )
+  const { questionNumber } = useParams();
+  const questionIndex = Number.parseInt(questionNumber);
+  const [questions, setQuestions] = useState({});
+  const [isDataLoading, setDataLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/survey').then((response) =>
+      response
+        .json()
+        .then(({ surveyData }) => {
+          setQuestions(surveyData);
+          setDataLoading(false);
+        })
+        .catch((error) => console.error(error))
+    );
+  }, []);
+  return (
+    <div>
+      <h1>Questionnaire 🧮</h1>
+      <h2>Question {questionIndex}</h2>
+      {isDataLoading ? <Loader /> : <p>{questions[questionIndex]}</p>}
+      <nav>
+        {questionIndex > 1 && (
+          <Link to={`/survey/${questionIndex - 1}`}>Précédente</Link>
+        )}
+        {questions[questionIndex + 1] ? (
+          <Link to={`/survey/${questionIndex + 1}`}>Suivante</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </nav>
+    </div>
+  );
 }
 
 export default Survey;
