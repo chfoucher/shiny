18fa70049055c2cac14d89d9a24124c02b45290e
chfoucher <pele.foucher@free.fr>
2023-01-27 08:53:29 +0100

    Ajout d'un contexte

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,41 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
 import Error from './components/Error';
+import Footer from './components/Footer';
 import Freelances from './pages/Freelances';
+import GlobalStyle from './utils/style/GlobalStyle';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Results from './pages/Results';
 import Survey from './pages/Survey';
+import { ThemeProvider } from './utils/context';
 
 import './index.css';
 
-const GlobalStyle = createGlobalStyle`
-  div {
-    font-family: 'Trebuchet MS', Helvetica, sans-serif;
-  }
-`;
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <GlobalStyle />
-    <Header />
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/survey/:questionNumber">
-        <Survey />
-      </Route>
-      <Route path="/results">
-        <Results />
-      </Route>
-      <Route path="/freelances/">
-        <Freelances />
-      </Route>
-      <Route>
-        <Error />
-      </Route>
-    </Switch>
+    <ThemeProvider>
+      <GlobalStyle />
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances/">
+          <Freelances />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </ThemeProvider>
   </Router>
 );
